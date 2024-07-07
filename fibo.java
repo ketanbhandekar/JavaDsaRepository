0f266492a8b44484f76ee16ef43467d55de050b5
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-07 14:38:45 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class fibo
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+
+		int n=sc.nextInt();
+
+		int a=0;
+		int b=1;
+
+		int cnt=2;
+
+		while(cnt<=n)
+		{
+			int temp=b;
+			b=b+a;
+			a=temp;
+
+			cnt++;
+		}
+		System.out.println(b);
+	}
+}
