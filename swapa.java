211098b8da8c4c374b95f113969342cfc0a825d9
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-11 20:32:59 +0530

    Add files via upload

---
+++ swapa.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Arrays;
+
+class swapa //Swap Values in an Array
+{
+	static void swap(int arr[],int index1, int index2)
+	{
+		int temp=arr[index1];
+		arr[index1]=arr[index2];
+		arr[index2]=temp;
+	}
+	public static void main(String args[])
+	{
+		int[]arr={1,2,3,4,5};
+		swap(arr,1,3);
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
