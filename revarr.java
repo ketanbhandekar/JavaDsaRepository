2e6fc2f6e5f644d9d244ceb23cfd85c643217404
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-12 20:25:25 +0530

    Update revarr.java

--- revarr.java
+++ revarr.java
@@ -26,3 +26,38 @@ class revarr
 		obj.rev();
 	}
 }
+
+	//WITH 2 POINTER METHOD
+// import java.io.*;
+// import java.lang.*;
+// import java.util.Arrays;
+
+// class revarray //Swap Values in an Array
+// {
+// 	static void swap(int arr[],int index1, int index2)
+// 	{
+// 		int temp=arr[index1];
+// 		arr[index1]=arr[index2];
+// 		arr[index2]=temp;
+// 	}
+// 	static void reverse(int arr[])
+// 	{
+// 		int start=0;
+// 		int end=arr.length-1;
+
+// 		while(start<end)
+// 		{
+// 			swap(arr,start,end);
+// 			start++;
+// 			end--;
+// 		}
+// 	}
+// 	public static void main(String args[])
+// 	{
+// 		int[]arr={1,2,3,4,5};
+// 		//swap(arr,0,4);
+// 		reverse(arr);
+
+// 		System.out.println(Arrays.toString(arr));
+// 	}
+// }
