3019c34c8e436ff3291c94a8bdfaf32685e1a240
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-12 20:38:46 +0530

    Update swapa.java

--- swapa.java
+++ swapa.java
@@ -6,9 +6,13 @@ class swapa //Swap Values in an Array
 {
 	static void swap(int arr[],int index1, int index2)
 	{
+		//int[]arr={1,2,3,4,5};
+		
 		int temp=arr[index1];
 		arr[index1]=arr[index2];
 		arr[index2]=temp;
+
+		//System.out.println(Arrays.toString(arr));
 	}
 	public static void main(String args[])
 	{
