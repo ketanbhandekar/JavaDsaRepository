94f0d27bfd7eab3a1d581a4876690fe881471383
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-12 18:12:07 +0530

    Update swapa.java

--- swapa.java
+++ swapa.java
@@ -18,3 +18,33 @@ class swapa //Swap Values in an Array
 		System.out.println(Arrays.toString(arr));
 	}
 }
+
+
+// import java.io.*;
+// import java.lang.*;
+// import java.util.Arrays;
+
+// class swapa //Swap Values in an Array
+// {
+// 	static void swap()
+// 	{
+// 		int arr[]={1,2,3,4,5};
+
+// 		int temp;
+
+// 		int index1=2;
+// 		int index2=3;
+
+// 		temp=arr[index1];
+// 		arr[index1]=arr[index2];
+// 		arr[index2]=temp;
+
+// 		System.out.println(Arrays.toString(arr));
+// 	}
+// 	public static void main(String args[])
+// 	{
+// 		swapa obj=new swapa();
+
+// 		obj.swap();
+// 	}
+// }
