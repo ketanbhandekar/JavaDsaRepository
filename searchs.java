45bf05cea53a07fa53a276c8199393b4d5344d21
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-16 21:36:47 +0530

    Add files via upload

---
+++ searchs.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.lang.*;
+
+class searchs 												//Search In String
+{
+	public boolean search()
+	{
+		String str="KB";
+		char target='B';
+
+		if(str.length()==0)
+		{
+			return false;
+		}
+
+		for(int i=0;i<str.length();i++)
+		{
+			if(target==str.charAt(i))
+			{
+				System.out.println("TARGET FOUND");
+
+				return true;
+			}
+		}
+		System.out.println("TARGET NOT FOUND");
+
+		return false;
+	}
+	public static void main(String args[])
+	{
+		searchs obj=new searchs();
+
+		obj.search();
+	}
+}
