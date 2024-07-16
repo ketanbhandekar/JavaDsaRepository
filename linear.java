d990b7b902a164c6173069e038b99eaa055a3e8c
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-16 22:43:32 +0530

    Update linear.java

--- linear.java
+++ linear.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.lang.*;
-import java.util.Arrays;
 
 class linear 								//Normal Linear Searching With Target Value
 {
@@ -42,3 +41,40 @@ class linear 								//Normal Linear Searching With Target Value
 //"TARGET ELEMENT NOT FOUND" message from the loop and print it only if the target element is not found after 
 //the loop finishes.
 
+
+
+// WITHOUT ELEMENT VARIALBE.....
+
+import java.io.*;
+import java.lang.*;
+
+class linear 								//Normal Linear Searching With Target Value
+{
+	public int search()						//public boolean search()
+	{
+		int arr[]={23,45,1,2,8,19,-3,16,-11,28};
+
+		int target=19;
+
+		for(int index=0;index<arr.length;index++)
+		{
+			//int element=arr[index];
+
+			if(arr[index]==target)
+			{
+				System.out.println("TARGET ELEMENT FOUND AT INDEX: " +index);
+
+				return 1;  				//return true;		//return index;
+			}
+		}
+		System.out.println("TARGET ELEMENT NOT FOUND");
+
+		return 0;						//return false;  	//return -1;
+	}
+	public static void main(String args[])
+	{
+		linear obj=new linear();
+
+		obj.search();
+	}
+}
