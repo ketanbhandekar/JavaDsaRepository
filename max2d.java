20cc9aa0fdaad3d5b83f86dc059b63c3c6d1e475
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-18 16:56:42 +0530

    Add files via upload

---
+++ max2d.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.lang.*;
+
+class max2d
+{
+	public void max()
+	{
+		int arr[][]={
+					{23,44,55},
+					{22,99,101},
+					{1,6,9},
+					};
+
+		//Imagine that arr is not empty
+
+		int max=arr[0][0];							//int max=0;				//(Starts From 0th Element OR
+																				//Starts From 0th Row)
+
+		for(int row=0;row<arr.length;row++)
+		{
+			for(int col=0;col<arr[row].length;col++)
+			{
+				if(arr[row][col]>max)
+				{
+					max=arr[row][col];
+				}
+			}
+
+		}
+		System.out.println("MAX: " +max);
+	}
+	public static void main(String args[])
+	{
+		max2d obj=new max2d();
+
+		obj.max();
+	}
+}
