75bd3fc3aca55c66e061c9c3119d9e32091f3903
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-18 15:07:26 +0530

    Update ip1.java

--- ip1.java
+++ ip1.java
@@ -43,3 +43,43 @@ class ip1
 		obj.input();
 	}
 }
+
+
+		//TAKING HARD VALUES FOR PRINTING  (WITHOUT TAKING INPUTS)
+
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class ip1
+{
+	public void input()
+	{
+		Scanner sc=new Scanner(System.in);
+
+		int arr[][]={
+					{1,2,3},
+					{4,5,6},
+					{7,8,9}
+					};
+
+		System.out.println("PRINTING THE INPUT: ");
+
+		System.out.println();
+
+		for(int row=0;row<arr.length;row++)
+		{
+			for(int col=0;col<arr[row].length;col++)
+			{
+				System.out.print(arr[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[])
+	{
+		ip1 obj=new ip1();
+
+		obj.input();
+	}
+}
