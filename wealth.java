386429f477e736d7da6022ef449fee8dd2e40fc9
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-19 20:06:41 +0530

    Add files via upload

---
+++ wealth.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.lang.*;
+
+class wealth
+{
+	public void money()
+	{
+		int accounts [][]={
+						  {1,2,3},
+						  {3,2,2},
+				  	      };
+		int ans=0;
+
+		for(int person=0;person<accounts.length;person++)
+		{
+			int sum=0;
+
+			for(int account=0;account<accounts[person].length;account++)
+			{
+				sum=sum+accounts[person][account];
+			}
+			if(sum>ans)
+			{
+				ans=sum;
+			}
+		}
+		System.out.println("ANSWER IS : "+ans);
+	}
+	public static void main(String args[])
+	{
+		wealth obj=new wealth();
+
+		obj.money();
+	}
+}
