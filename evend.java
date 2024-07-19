bd5c0e7b714f614b2654bf0ffaffb790ed9557c2
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-19 19:36:19 +0530

    Add files via upload

---
+++ evend.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.lang.*;
+
+class evend									//even digit
+{
+	public void find()
+	{
+		int arr[]={12,345,2,6};
+
+		int cnt=0;
+
+		int ans=0;							//to Store the numbers in an Array
+
+		for(int i=0;i<arr.length;i++)
+		{
+			cnt=0;
+
+			while(arr[i]>0)
+			{
+				cnt++;						//counting the digits
+
+				arr[i]=arr[i]/10;			//reducing the digits
+			}
+			if(cnt%2==0)
+			{
+				ans++;						//stores even numbers in an array
+			}
+		}
+		System.out.println(ans);
+	}
+	public static void main(String args[])
+	{
+		evend obj=new evend();
+
+		obj.find();
+	}
+}
