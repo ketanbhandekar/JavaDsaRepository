801f51382b942335e63bd0f471c687b49597d3f4
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-19 23:41:30 +0530

    Add files via upload

---
+++ binarysearch.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.lang.*;
+
+class binarysearch
+{
+	public void search()
+	{
+		int arr[]={-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
+
+		int target=22;
+
+		int start=0;
+
+		int end=arr.length-1;
+
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+
+			if(target>arr[mid])
+			{
+				start=mid+1;
+			}
+			else if(target<arr[mid])
+			{
+				end=mid-1;
+			}
+			else
+			{
+				System.out.println("TARGET FOUND: " +arr[mid]);
+
+				return;
+			}
+		}
+		System.out.println("TARGET NOT FOUND");
+	}
+	public static void main(String args[])
+	{
+		binarysearch obj=new binarysearch();
+
+		obj.search();
+	}
+}
+
