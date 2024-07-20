4256aea5ebe514160902694c0d9ef0f915fc7f03
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-21 00:44:11 +0530

    Add files via upload

---
+++ floorbs.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.lang.*;
+
+class floorbs
+{
+	public void search()
+	{
+		//return the index of greatest no >= target
+
+		int arr[]={2,3,5,9,14,16,18};
+
+		int target=15;
+
+		int start=0;
+
+		int end=arr.length-1;
+
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+
+			if(target>arr[mid])
+			{
+				start=mid+1;
+			}
+			else if(target<arr[mid])
+			{
+				end=mid-1;
+			}
+			else
+			{
+				System.out.println("TARGET FOUND: " +arr[mid]);
+
+				return;
+			}
+		}
+		System.out.println(arr[end]);
+	}
+	public static void main(String args[])
+	{
+		floorbs obj=new floorbs();
+
+		obj.search();
+	}
+}
