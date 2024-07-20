80e3186a4b257042dc148001b7785b25acbf23b0
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-07-21 00:02:56 +0530

    Add files via upload

---
+++ ceilingbs.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.lang.*;
+
+class ceilingbs
+{
+	//return the index of smalled no >= target
+	public void search()
+	{
+		int arr[]={2,3,5,9,14,16,18};
+
+		int target=15;
+
+		int start=0;
+
+		int end=arr.length-1;
+
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+
+			if(target>arr[mid])
+			{
+				start=mid+1;
+			}
+			else if(target<arr[mid])
+			{
+				end=mid-1;
+			}
+			else
+			{
+				System.out.println("TARGET FOUND: " +arr[mid]);
+
+				return;
+			}
+		}
+		System.out.println(arr[start]);
+	}
+	public static void main(String args[])
+	{
+		ceilingbs obj=new ceilingbs();
+
+		obj.search();
+	}
+}
+
