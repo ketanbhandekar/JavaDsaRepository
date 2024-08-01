4b2771afd0603ded9b2ba82762368c61a231e05a
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-01 16:57:57 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.lang.*;
+
+class pattern3
+{
+	public void pat()
+	{
+		int row=0;
+		int col=0;
+
+		for(row=5;row>=1;row--)
+		{
+			for(col=1;col<=row;col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[])
+	{
+		pattern3 obj=new pattern3();
+
+		obj.pat();
+	}
+}
+
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
