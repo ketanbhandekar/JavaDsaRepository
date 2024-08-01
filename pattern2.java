5e16de6090ed088d81f30a5d50ee5d67fad373eb
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-01 15:09:48 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.lang.*;
+
+class pattern2
+{
+	public void pat()
+	{
+		int row=0;
+		int col=0;
+
+		for(row=1;row<=6;row++)
+		{
+			for(col=1;col<=6;col++)
+			{
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[])
+	{
+		pattern2 obj=new pattern2();
+
+		obj.pat();
+	}
+}
+
+// 							*  *  *  *  *  *
+//              			*  *  *  *  *  *
+//							*  *  *  *  *  *
+//							*  *  *  *  *  *
+//							*  *  *  *  *  *
+//							*  *  *  *  *  *
