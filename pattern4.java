871697aad4f8f0077500ee4c0898969cae053216
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-01 17:34:37 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.*;
+
+class pattern4
+{
+	public void pat()
+	{
+		int row=0;
+		int col=0;
+
+		for(row=1;row<=5;row++)
+		{
+			for(col=1;col<=row;col++)
+			{
+				System.out.print(col );
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[])
+	{
+		pattern4 obj=new pattern4();
+
+		obj.pat();
+	}
+}
+
+//			1
+//			12
+//			123
+//			1234
+//			12345
+
+
