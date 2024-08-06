c9e3a47b5a43cd1a22d6431a546499934646bbd7
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-06 18:28:36 +0530

    Add files via upload

---
+++ fiborecursion.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.lang.*;
+
+class fiborecursion
+{
+	static int fibo(int n)
+	{
+	    if(n == 0)
+	    {
+	        return 0;
+	  	}
+	    else if(n == 1)
+	    {
+	      return 1;
+	 	}
+	    else
+	    {
+	      return fibo(n - 1) + fibo(n - 2);
+	 	}
+	}
+	public static void main(String args[])
+	{
+		fiborecursion obj=new fiborecursion();
+
+		int ans=fibo(6);
+
+		System.out.println(ans);
+	}
+}
