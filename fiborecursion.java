1cb43ba0f03f5bb3b473cb5337a0d6936efe4147
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-06 18:30:44 +0530

    Update fiborecursion.java

--- fiborecursion.java
+++ fiborecursion.java
@@ -7,16 +7,16 @@ class fiborecursion
 	{
 	    if(n == 0)
 	    {
-	        return 0;
-	  	}
+		return 0;
+	    }
 	    else if(n == 1)
 	    {
 	      return 1;
-	 	}
-	    else
+	    }
+	   else
 	    {
 	      return fibo(n - 1) + fibo(n - 2);
-	 	}
+	    }
 	}
 	public static void main(String args[])
 	{
