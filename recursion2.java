ccc5ddcfa8f96d9c65dd7e99882c9ce9ce791800
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-08 00:51:05 +0530

    Update recursion2.java

--- recursion2.java
+++ recursion2.java
@@ -1,14 +1,13 @@
 import java.io.*;
 import java.lang.*;
-							//Q.A Function that takes in a no & prints it , print first 5 numbers
+					//Q.A Function that takes in a no & prints it , print first 5 numbers
 class recursion2
 {
 	public void print(int n)
 	{
-		if(n==5)
+		if(n==6)		//Because We Have to print till 5 So,
 		{
-			System.out.println(5);
-
+			//System.out.println(5);
 			return;
 		}
 		System.out.println(n);
