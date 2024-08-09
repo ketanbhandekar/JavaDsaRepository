51ba115c1ada5f5a1b4291ba7b70db30d0d89f7f
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-09 23:53:12 +0530

    Update ReverseRecurOne.java

--- ReverseRecurOne.java
+++ ReverseRecurOne.java
@@ -1,6 +1,43 @@
 import java.io.*;
 import java.lang.*;
 
+class main
+{
+	public static void Reverse(int num)
+	{
+        // base condition to end recursive calls
+        if (num < 10)
+        {
+            System.out.println(num);
+
+         	return;
+        }
+        else
+        {
+            // print the unit digit of the given number
+            System.out.print(num % 10);
+            // calling function for remaining number other than unit digit
+            Reverse(num / 10);
+        }
+    }
+    // driver code
+    public static void main(String args[])
+    {
+        // number to be reversed
+        int num = 100;
+
+        System.out.print("Reversed Number: ");
+        // calling recursive function to print the number in reversed form
+        Reverse(num);
+    }
+}
+
+
+
+
+import java.io.*;					
+import java.lang.*;
+
 class ReverseRecurOne
 {
 	static int rev(int no,int rev)
