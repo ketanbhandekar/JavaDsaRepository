f85c66f6a403c6b8f39d5db3a427d868a997ef41
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-11 00:30:58 +0530

    Update SortArrRecur.java

--- SortArrRecur.java
+++ SortArrRecur.java
@@ -7,11 +7,12 @@ class SortArrRecur
 
 	static boolean sorted(int arr[],int index)
 	{
-		if(index==arr.length-1)
+		if(index==arr.length-1)					//Base Case
 		{
 			return true;
 		}
-		return arr[index]<arr[index+1] && sorted(arr,index+1);
+		return arr[index]<arr[index+1] && sorted(arr,index+1);		//This line compares the current element with the next element.
+							//If the current element is less than the next one, it recursively checks the next pair of elements by calling the sorted method with the next index.
 	}
 	public static void main(String args[])
 	{
