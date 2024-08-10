05feecb3c1ff24fde32b905e43e5c65dc4f54186
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-10 18:03:19 +0530

    Update CountZeroRecursion.java

--- CountZeroRecursion.java
+++ CountZeroRecursion.java
@@ -1,6 +1,37 @@
 import java.io.*;
 import java.lang.*;
 
+class ZeroRecur
+{
+	static int count(int n)
+	{
+		return helper(n,0);
+	}
+	static int helper(int n,int c)
+	{
+		if(n==0)
+		{
+			return c;
+		}
+		int rem=n%10;
+
+		if(rem==0)
+		{
+			return helper(n/10,c+1);
+		}
+		return helper(n/10,c);
+	}
+	public static void main(String args[])
+	{
+		System.out.println(count(10204));
+	}
+}
+
+
+
+import java.io.*;
+import java.lang.*;
+
 class CountZeroRecursion
 {
 	public static int countZerosRec(int input)
