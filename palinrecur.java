bbb68b83be01b83340129f7a4887c9af41bd4438
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-10 16:38:30 +0530

    Add files via upload

---
+++ palinrecur.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class palinrecur
+{
+	static void pal(int no)
+	{
+		if(no==0)
+		{
+			return;
+		}
+		int x=0;
+		int rem=0;
+		int rev=0;
+
+		x=no;
+
+		pal(no/10);
+
+		if(x==no)
+		{
+			System.out.println("PALIN");
+		}
+		else
+		{
+			System.out.println("NOT PALIN");
+		}
+	}
+	public static void main(String args[])
+	{
+		palinrecur obj=new palinrecur();
+
+		obj.pal(1221);
+	}
+}
+
