f9a5bfbbaf124888d490837124325852bc38877c
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-11 17:59:15 +0530

    Add files via upload

---
+++ LinearRecur.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.lang.*;
+
+class LinearRecur
+{
+	static int arr[]={1,4,7,9,11};
+
+	static int target=9;
+
+	static int search(int arr[],int target,int index)
+	{
+		if(arr[index]==target)
+		{
+			return index;
+		}
+		else
+		{
+			return search(arr,target,index+1);
+		}
+	}
+	public static void main(String args[])
+	{
+		LinearRecur obj=new LinearRecur();
+
+		System.out.println(search(arr,target,0));
+	}
+}
