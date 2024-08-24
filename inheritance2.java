09bc7a2cc405528f2b5f2cb68065530f2635941f
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-24 20:04:04 +0530

    Add files via upload

---
+++ inheritance2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class k
+{
+	int rollno;
+
+	public void info()
+	{
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("ENTER THE ROLLNO: ");
+		rollno=sc.nextInt();
+	}
+}
+class b extends k
+{
+	int id;
+
+	public void info()
+	{
+		super.info();
+
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("ENTER THE ID: ");
+		id=sc.nextInt();
+
+		System.out.println("---------------------------------");
+		System.out.println("ROLLNO: " + rollno);
+		System.out.println("ID: " +id);
+	}
+}
+class inheritance2
+{
+	public static void main(String args[])
+	{
+		b obj=new b();
+
+		obj.info();
+	}
+}
