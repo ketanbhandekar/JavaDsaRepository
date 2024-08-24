dfaeac7cb1dae2694a98371b560b9d5288f45231
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-24 19:01:01 +0530

    Add files via upload

---
+++ inheritance1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class base     //parent class
+{
+	int a;
+	int b;
+}
+class child extends base
+{
+	public void sum()										//child()
+	{
+		int ans;
+
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter The Value of A: ");
+		a=sc.nextInt();
+
+		System.out.println("Enter The Value of B: ");
+		b=sc.nextInt();
+
+		ans=a+b;
+
+		System.out.println("ADDITION OF 2 NUMBERS : " +ans);
+	}
+}
+class inheritance1
+{
+	public static void main(String args[])
+	{
+		child obj=new child();
+
+		obj.sum();
+	}
+}
+
+
