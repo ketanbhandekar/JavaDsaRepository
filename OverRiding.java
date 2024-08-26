bbc80b7050dd2b8e236245ca8db2731afd1c4c65
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-08-26 16:25:12 +0530

    Add files via upload

---
+++ OverRiding.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.lang.*;
+
+class P
+{
+	public void greet()
+	{
+		System.out.println("HELLO");
+	}
+}
+class C extends P
+{
+	public void greet()
+	{
+		System.out.println("HELLLLO");
+	}
+}
+class OverRiding								//child-class has same method as declared in parent class.
+{
+	public static void main(String args[])
+	{
+		C obj=new C();
+
+		obj.greet();
+	}
+}
