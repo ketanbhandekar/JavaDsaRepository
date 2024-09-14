9bbd0330c79254fd52ac27ba0268c6c94031e4bf
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-09-14 16:34:45 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+
+class exception
+{
+	public static void main(String argsp[])
+	{
+		int a=10;
+		int b=0;
+
+		try
+		{
+			int c=a/b;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
